2a0a2b43d8b0e30b33b54644f0fa6b063041c30d
sahal <sahal@Sahalpk>
2025-07-21 16:30:11 +0530

    Updated

--- C2TC_CoreJavaProgram/src/day1/entity/Customer.java
+++ C2TC_CoreJavaProgram/src/day1/entity/Customer.java
@@ -1,7 +1,45 @@
 package day1.entity;
 
 public class Customer {
-	int id;
-	String cname;
-	String city;
+	 private int id;
+	private String cname;	
+	private String city;
+	
+	
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getCname() {
+		return cname;
+	}
+
+
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+
+
+	public String getCity() {
+		return city;
+	}
+
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", cname=" + cname + ", city=" + city + "]";
+	}
+	
+	
 }
--- C2TC_CoreJavaProgram/src/day1/entity/CustomerDemo.java
+++ C2TC_CoreJavaProgram/src/day1/entity/CustomerDemo.java
@@ -3,13 +3,30 @@ package day1.entity;
 public class CustomerDemo {
 
 	public static void main(String[] args) {
+		
+		//getter and setter
+		
+		System.out.println("Getter and Setter method");
+		
 		Customer c1=new Customer();
-		c1.id=101;
-		c1.cname="Raju";
-		c1.city="PDY";
-		System.out.println("Customer Id: "+c1.id);
-		System.out.println("Customer Id: "+c1.cname);
-		System.out.println("Customer Id: "+c1.city);
+		c1.setId(101);
+		c1.setCname("Raju");
+		c1.setCity("PDY");
+		System.out.println("Customer Id: "+c1.getId());
+		System.out.println("Customer Id: "+c1.getCname());
+		System.out.println("Customer Id: "+c1.getCity());
+		
+		//toString
+		
+		System.out.println("toString method");
+		
+		Customer c2=new Customer();
+		c2.setId(102);
+		c2.setCname("Manu");
+		c2.setCity("VPM");
+		
+		System.out.println(c2);
+		
 
 	}
  
