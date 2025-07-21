b25bfb738ad37b42b6cf535b8033fb82bf7d1c5a
sahal <sahal@Sahalpk>
2025-07-21 15:47:03 +0530

    entity

---
+++ C2TC_CoreJavaProgram/src/day1/entity/Customer.java
@@ -0,0 +1,7 @@
+package day1.entity;
+
+public class Customer {
+	int id;
+	String cname;
+	String city;
+}
---
+++ C2TC_CoreJavaProgram/src/day1/entity/CustomerDemo.java
@@ -0,0 +1,16 @@
+package day1.entity;
+
+public class CustomerDemo {
+
+	public static void main(String[] args) {
+		Customer c1=new Customer();
+		c1.id=101;
+		c1.cname="Raju";
+		c1.city="PDY";
+		System.out.println("Customer Id: "+c1.id);
+		System.out.println("Customer Id: "+c1.cname);
+		System.out.println("Customer Id: "+c1.city);
+
+	}
+ 
+}
