c8294ed7bdf2a8710adb8a7732dc8fa5876220b9
sahal <sahal@Sahalpk>
2025-07-21 15:01:03 +0530

    Addition

--- C2TC_CoreJavaProgram/src/day1/basic/SampleDemo.java
+++ C2TC_CoreJavaProgram/src/day1/basic/SampleDemo.java
@@ -2,11 +2,19 @@ package day1.basic;
 
 public class SampleDemo {
 
+	static {
+		System.out.println("block 2...");
+		
+	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to JFS...");
 		System.out.println("Sahalpk.....");
 		System.out.println("hello.....");
 
+	}
+	static {
+		System.out.println("block 1...");
+		
 	}
 
 }
