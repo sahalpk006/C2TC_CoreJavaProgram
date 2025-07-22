22b236803ac79b39f0c0e92aaccb9d256276fda2
sahal <sahal@Sahalpk>
2025-07-22 18:34:46 +0530

    constructor

---
+++ C2TC_CoreJavaProgram/src/day1/constructor/CustomerDemo.java
@@ -0,0 +1,16 @@
+package day1.constructor;
+
+public class CustomerDemo {
+
+	public static void main(String[] args) {
+		Customer c1=new Customer();
+		
+		System.out.println(c1);
+		
+		
+		Customer c2=new Customer(102);
+		System.out.println(c2);
+
+	}
+
+}
