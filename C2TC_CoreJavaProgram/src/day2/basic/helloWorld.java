f2fe802f964c178ea6e02d34901df07d308f8b73
sahal <sahal@Sahalpk>
2025-07-22 18:33:28 +0530

    day-2 basic

---
+++ C2TC_CoreJavaProgram/src/day2/basic/helloWorld.java
@@ -0,0 +1,15 @@
+package day2.basic;
+
+import java.util.Scanner;
+
+public class helloWorld {
+
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		System.out.println("ENter YOur name : ");
+		String n=input.next();
+		System.out.println("Hello "+n);
+
+	}
+
+}
