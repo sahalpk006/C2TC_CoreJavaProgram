bc423f234996f77fae5c0432a5aff038b9e60bea
sahal <sahal@Sahalpk>
2025-07-24 14:28:25 +0530

    day-Four

---
+++ C2TC_CoreJavaProgram/src/dayfour/secondpackage/Executor.java
@@ -0,0 +1,16 @@
+package dayfour.secondpackage;
+
+public class Executor {
+
+	public static void main(String[] args) {
+		base b1 = new base();
+
+		
+
+		b1.methodPublic();
+		b1.varPublic = 41;
+		b1.methodPublic();
+
+	}
+
+}
