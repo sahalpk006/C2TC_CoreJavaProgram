3c43be9c616f66ec856c7f0b116e3f23e7a0b1ab
sahal <sahal@Sahalpk>
2025-07-24 20:38:16 +0530

    comit

---
+++ C2TC_CoreJavaProgram/src/dayfive/hierarchicalinheritance/Employee.java
@@ -0,0 +1,28 @@
+package dayfive.hierarchicalinheritance;
+
+public class Employee extends Person{
+	private int empId;
+	private float salary;
+	private String dept;
+	
+	
+	public Employee() {
+		System.out.println("Employee Class Default Constructor");
+	}
+
+
+	public Employee(int empId, float salary, String dept) {
+		this.empId = empId;
+		this.salary = salary;
+		this.dept = dept;
+	}
+	
+	
+	public Employee(String name, String city,int empId, float salary, String dept) {
+		super(name,city);
+		this.empId = empId;
+		this.salary = salary;
+		this.dept = dept;
+	}
+
+}
