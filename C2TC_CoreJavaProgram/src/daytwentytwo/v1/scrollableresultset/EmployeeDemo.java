5d84a1c1c68f345fcdc76e278ce40a041746f87c
sahal <sahal@Sahalpk>
2025-08-08 14:30:25 +0530

    comit

---
+++ C2TC_CoreJavaProgram/src/daytwentytwo/v1/scrollableresultset/EmployeeDemo.java
@@ -0,0 +1,23 @@
+package daytwentytwo.v1.scrollableresultset;
+
+import java.sql.SQLException;
+
+public class EmployeeDemo {
+
+	public static void main(String[] args) {
+		try {
+			EmployeeService.showFirst();
+			EmployeeService.showLast();
+			EmployeeService.showPrevious();
+			EmployeeService.showPrevious();
+			EmployeeService.showNext();
+			EmployeeService.updateSalary(65000);
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
